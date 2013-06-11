69809d25e561370c3e2cd0b5aad22f1cf92c325b
Paul Lavoine <user@x174>
2013-06-11 22:18:54 +0200

    module prettify

--- ext/prettify.c
+++ ext/prettify.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
 #include <dlfcn.h>
-#include "unistd.h"
+#include <unistd.h>
+#include <string.h>
 #include "../include/http.h"
 #include "../include/handler.h"
 #include "../include/misc.h"
+#include "../include/network.h"
+
+#define MAX 1024
+
 
 int handler(struct http_request *req){
-	//printf("rres : %s OK",req->uri); // affiche ca : "rres : /ext/simple-module.c OKrres : /ext/simple-module.c OK"
+	char adresse[MAX]="";
+	strcpy(adresse,document_root);
+	strcat(adresse,req->uri);
+	int fdout = fileno(req->ci->fout);
+	dup2(fdout,1);
+	close(fdout);
 
-	execlp(req->uri,req->uri, "-f html", "Ofull", "simple-module.c",NULL);
+	if(strstr(req->query_string , "fontify")!=NULL){
+		execlp("/usr/bin/pygmentize","/usr/bin/pygmentize", "-f", "html", "-Ofull", adresse,NULL);
+		return 1;
+	}
+	if(strstr(req->query_string , "markdown")!=NULL){
+		execlp("/usr/bin/pandoc","/usr/bin/pandoc", "-s", adresse,NULL);
+		return 1;
+	}
+
 	return 0;
 }
 
 void init_module(void)
 {	
-	trace("bouboul\n");
+	trace("Loading .. \n");
 	handler_uri_add(handler);
 }
 
